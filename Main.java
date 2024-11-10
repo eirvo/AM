f2e8289a390d5e813df84c14b6fd6d692759e215
christinasiakavara <92878436+christinasiakavara@users.noreply.github.com>
2024-11-10 22:09:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("8230040");
         System.out.println("8230123");
         System.out.println("8230016");
+        System.out.println("8200152");
     }
     
 }
