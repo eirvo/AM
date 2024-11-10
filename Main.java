0d9359e2f8ae50c7f1d731e57af2f0205a96340c
anastasia-th <t8230040@aueb.gr>
2024-11-10 20:09:31 +0200

    test2

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("8230040");
-        System.out.println("1")
+        System.out.println("12")
     }
     
 }
