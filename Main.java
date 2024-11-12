4729375dc227808a7c3ef97850cda57ed57df41f
t8220046 <t8220046@aueb.gr>
2024-11-12 17:53:58 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("8200152");
         System.out.println("8220091");
         System.out.println("8210220");
+        System.out.println("8220046");
     }
     
 }
